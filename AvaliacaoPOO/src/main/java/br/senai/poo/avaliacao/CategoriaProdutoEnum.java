72d9be2c8c546aa0dfd11781adabd907099d3ba6
Victor Hugo <hugo16domingos@outlook.com>
2025-05-22 22:09:12 -0300

    Delete AvaliacaoPOO directory

--- AvaliacaoPOO/src/main/java/br/senai/poo/avaliacao/CategoriaProdutoEnum.java
+++
@@ -1,7 +0,0 @@
-package br.senai.poo.avaliacao;
-
-public enum CategoriaProdutoEnum {
-
-    //Enumeração
-    ELETRONICO, ROUPA, ALIMENTO;
-}
--- AvaliacaoPOO/src/main/java/br/senai/poo/avaliacao/ICalculavel.java
+++
@@ -1,6 +0,0 @@
-package br.senai.poo.avaliacao;
-
-//Interface
-public interface ICalculavel {
-    public double calcularPrecoFinal();
-}
